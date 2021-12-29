025a75aaafad1041b6046cd2dd5cb0ae265c8a28
maithreyan <maithreyanm8@gmail.com>
2021-12-29 23:32:36 +0530

    new pade created

--- resume.js
+++ resume.js
@@ -2,4 +2,10 @@ var loadFile = function (event) {
     var image = document.getElementById("output");
     image.src = URL.createObjectURL(event.target.files[0]);
   };
-  
+
+  function myFunction() {
+    let person = prompt("Please enter your name", "Harry Potter");
+    if (person != null) {
+      document.getElementById("demo").innerHTML = person ;
+    }
+  }
