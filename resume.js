076341c79e7147be1682d49bef5851695134bf60
maithreyan <maithreyanm8@gmail.com>
2022-01-01 23:03:28 +0530

    new pade created

--- resume.js
+++ resume.js
@@ -3,9 +3,21 @@ var loadFile = function (event) {
     image.src = URL.createObjectURL(event.target.files[0]);
   };
 
-  function myFunction() {
-    let person = prompt("Please enter your name", "Harry Potter");
-    if (person != null) {
-      document.getElementById("demo").innerHTML = person ;
-    }
+  function getData()
+  {
+      //gettting the values
+      var email = document.getElementById("email").value;
+      var password= document.getElementById("password").value; 
+      var telephone= document.getElementById("telephone").value; 
+      var mobile= document.getElementById("mobile").value; 
+      //saving the values in local storage
+      localStorage.setItem("txtValue", email);
+      localStorage.setItem("txtValue1", password);
+      localStorage.setItem("txtValue2", mobile);
+      localStorage.setItem("txtValue3", telephone);   
   }
+
+  document.getElementById("data").innerHTML=localStorage.getItem("txtValue");
+document.getElementById("data1").innerHTML=localStorage.getItem("txtValue1");
+document.getElementById("data2").innerHTML=localStorage.getItem("txtValue2");
+document.getElementById("data3").innerHTML=localStorage.getItem("txtValue3");
