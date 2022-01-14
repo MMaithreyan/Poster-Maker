395871902e4dcb2181f561e4206204bb3b686a94
maithreyan <maithreyanm8@gmail.com>
2022-01-14 23:18:01 +0530

    profile completed

--- resume.js
+++ resume.js
@@ -13,6 +13,10 @@ function getData() {
   var mobile = document.getElementById("mobile").value;
   var id = document.getElementById("id").value;
   var address = document.getElementById("Address").value;
+  var skill1 = document.getElementById("skill1").value;
+  var skill2 = document.getElementById("skill2").value;
+  var skill3 = document.getElementById("skill3").value;
+  var skill4 = document.getElementById("skill4").value;
 
   //saving the values in local storage
   localStorage.setItem("txtValue", email);
@@ -21,6 +25,11 @@ function getData() {
   localStorage.setItem("txtValue3", telephone);
   localStorage.setItem("txtValue4", id);
   localStorage.setItem("txtValue5", address);
+  localStorage.setItem("txtValue6", skill1);
+  localStorage.setItem("txtValue7", skill2);
+  localStorage.setItem("txtValue8", skill3);
+  localStorage.setItem("txtValue9", skill4);
+
 }
 
 
@@ -30,3 +39,8 @@ document.getElementById("data2").innerHTML = localStorage.getItem("txtValue2");
 document.getElementById("data3").innerHTML = localStorage.getItem("txtValue3");
 document.getElementById("data4").innerHTML = localStorage.getItem("txtValue4");
 document.getElementById("data5").innerHTML = localStorage.getItem("txtValue5");
+document.getElementById("data6").innerHTML = localStorage.getItem("txtValue6");
+document.getElementById("data7").innerHTML = localStorage.getItem("txtValue7");
+document.getElementById("data8").innerHTML = localStorage.getItem("txtValue8");
+document.getElementById("data9").innerHTML = localStorage.getItem("txtValue9");
+
