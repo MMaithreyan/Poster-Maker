30656180e6a382bbe2578171e5f7d9865ed5a18b
MMaithreyan <maithreyanm8@gmail.com>
2022-02-08 00:28:57 +0530

    pic added

--- resume.js
+++ resume.js
@@ -3,3 +3,10 @@ var loadFile = function (event) {
   var image = document.getElementById("output");
   image.src = URL.createObjectURL(event.target.files[0]);
 };
+
+function getData(){
+  var company_name = document.getElementById('company_name').value;
+
+  localStorage.setItem("company_name",company_name);
+}
+document.getElementById('data1').innerHTML=localStorage.getItem('company_name')
