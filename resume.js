cbd90f3b184f4c5a73446ae392eb0145e4754e15
MMaithreyan <maithreyanm8@gmail.com>
2022-02-08 23:55:14 +0530

    pic added

--- resume.js
+++ resume.js
@@ -1,12 +1,15 @@
-
-var loadFile = function (event) {
+var loadfile = function (event) {
   var image = document.getElementById("output");
   image.src = URL.createObjectURL(event.target.files[0]);
 };
 
-function getData(){
-  var company_name = document.getElementById('company_name').value;
 
+function getData(){
+  var post = document.getElementById('file');
+  var company_name = document.getElementById('company_name').value;
+  localStorage.setItem("post",post);
   localStorage.setItem("company_name",company_name);
+
 }
-document.getElementById('data1').innerHTML=localStorage.getItem('company_name')
+document.getElementById('data1').innerHTML=localStorage.getItem('company_name');
+document.getElementById('output')=localStorage.getItem('post');
