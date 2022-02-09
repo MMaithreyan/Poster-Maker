a8db4a0baf7dd6a0f5f2cfe725adad1cb6b61718
MMaithreyan <maithreyanm8@gmail.com>
2022-02-10 00:38:54 +0530

    pic added

--- resume.js
+++ resume.js
@@ -11,6 +11,7 @@ function getData(){
   var speaker = document.getElementById('speaker').value;
   var about_speaker = document.getElementById('about_speaker').value;
   var date = document.getElementById('date').value;
+  var time = document.getElementById('time').value;
   var meet = document.getElementById('meet').value;
   var link = document.getElementById('link').value;
 
@@ -20,6 +21,7 @@ function getData(){
   localStorage.setItem("speaker",speaker);
   localStorage.setItem("about_speaker",about_speaker);
   localStorage.setItem("date",date);
+  localStorage.setItem("time",time);
   localStorage.setItem("meet",meet);
   localStorage.setItem("link",link);
 
@@ -30,5 +32,6 @@ document.getElementById('data3').innerHTML=localStorage.getItem('about');
 document.getElementById('data4').innerHTML=localStorage.getItem('speaker');
 document.getElementById('data5').innerHTML=localStorage.getItem('about_speaker');
 document.getElementById('data6').innerHTML=localStorage.getItem('date');
+document.getElementById('data9').innerHTML=localStorage.getItem('time');
 document.getElementById('data7').innerHTML=localStorage.getItem('meet');
 document.getElementById('data8').innerHTML=localStorage.getItem('link');
